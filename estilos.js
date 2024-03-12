6d7be414382ad6bc0d73f964d56c2ba619800c33
flavianarumi <naruminitto123@gmail.com>
2024-03-12 09:02:29 -0300

    add estilos

---
+++ estilos.js
@@ -0,0 +1,12 @@
+import { Text, StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.creat({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default styles;
